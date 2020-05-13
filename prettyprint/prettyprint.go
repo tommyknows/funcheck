c15f4ec19900ebd410468ccbec5b02e50d8d2efe
Ramon Rüttimann <ramon@nine.ch>
2020-05-13 17:28:52 +0200

    Rewrite prettyprint to comply to funcheck standards
    Signed-off-by: Ramon Rüttimann <ramon@nine.ch>

--- prettyprint/prettyprint.go
+++ prettyprint/prettyprint.go
@@ -16,64 +16,72 @@ var Analyzer = &analysis.Analyzer{
 	Run:  run,
 }
 
+type null struct{}
+
+func checkDecl(as *ast.DeclStmt, fset *token.FileSet) {
+	fmt.Printf("Declaration %q: %v\n", render(fset, as), as.Pos())
+
+	check := func(_ null, spec ast.Spec) (n null) {
+		val, ok := spec.(*ast.ValueSpec)
+		if !ok {
+			return
+		}
+		if val.Values != nil {
+			return
+		}
+		if _, ok := val.Type.(*ast.FuncType); !ok {
+			return
+		}
+		fmt.Printf("\tIdent %q: %v\n", render(fset, val), val.Names[0].Pos())
+		return
+	}
+
+	if decl, ok := as.Decl.(*ast.GenDecl); ok {
+		_ = foldl(check, null{}, decl.Specs)
+	}
+}
+
+func checkAssign(as *ast.AssignStmt, fset *token.FileSet) {
+	fmt.Printf("Assignment %q: %v\n", render(fset, as), as.Pos())
+
+	check := func(_ null, expr ast.Expr) (n null) {
+		ident, ok := expr.(*ast.Ident) // Lhs always is an "IdentifierList"
+		if !ok {
+			return
+		}
+
+		fmt.Printf("\tIdent %q: %v\n", ident.String(), ident.Pos())
+
+		switch {
+		case ident.Name == "_":
+			fmt.Printf("\t\tBlank Identifier!\n")
+		case ident.Obj == nil:
+			fmt.Printf("\t\tDecl is not in the same file!\n")
+		default:
+			// make sure the declaration has a Pos func and get it
+			declPos := ident.Obj.Decl.(ast.Node).Pos()
+			fmt.Printf("\t\tDecl %q: %v\n", render(fset, ident.Obj.Decl), declPos)
+		}
+
+		return
+	}
+	_ = foldl(check, null{}, as.Lhs)
+}
+
 func run(pass *analysis.Pass) (interface{}, error) {
-	for _, file := range pass.Files {
+	inspect := func(_ null, file *ast.File) (n null) {
 		ast.Inspect(file, func(n ast.Node) bool {
 			switch as := n.(type) {
 			case *ast.DeclStmt:
-				fmt.Printf("Declaration %q: %v\n", render(pass.Fset, as), as.Pos())
-				decl, ok := as.Decl.(*ast.GenDecl)
-				if !ok {
-					break
-				}
-
-				for i := range decl.Specs {
-					val, ok := decl.Specs[i].(*ast.ValueSpec)
-					if !ok {
-						continue
-					}
-
-					if val.Values != nil {
-						continue
-					}
-
-					if _, ok := val.Type.(*ast.FuncType); !ok {
-						continue
-					}
-
-					fmt.Printf("\tIdent %q: %v\n", render(pass.Fset, val), val.Names[0].Pos())
-				}
+				checkDecl(as, pass.Fset)
 			case *ast.AssignStmt:
-				type pos interface {
-					Pos() token.Pos
-				}
-
-				fmt.Printf("Assignment %q: %v\n", render(pass.Fset, as), as.Pos())
-
-				for _, expr := range as.Lhs {
-					ident := expr.(*ast.Ident) // Lhs always is an "IdentifierList"
-
-					fmt.Printf("\tIdent %q: %v\n", ident.String(), ident.Pos())
-
-					// skip blank identifiers
-					if ident.Name == "_" {
-						fmt.Printf("\t\tBlank Identifier!\n")
-						continue
-					}
-
-					if ident.Obj == nil {
-						fmt.Printf("\t\tDecl is not in the same file!\n")
-						continue
-					}
-
-					// make sure the declaration has a Pos func and get it
-					declPos := ident.Obj.Decl.(pos).Pos()
-					fmt.Printf("\t\tDecl %q: %v\n", render(pass.Fset, ident.Obj.Decl), declPos)
-				}
+				checkAssign(as, pass.Fset)
 			}
 			return true
 		})
+		return
 	}
+	_ = foldl(inspect, null{}, pass.Files)
 
 	return nil, nil
 }
